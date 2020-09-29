3024fb39ee83e0f242d6be9142c3854103284179
AkashShanker <akash.shanker12@gmail.com>
2020-09-29 06:39:26 +0000

    Cleaned up main script

--- openssl.sh
+++ openssl.sh
@@ -1,55 +1,38 @@
-yum install gcc cmake
-
-mkdir ~/openssl-install && cd ~/openssl-install
-#wget https://www.openssl.org/source/openssl-1.1.1g.tar.gz
-curl -O -L https://github.com/openssl/openssl/archive/OpenSSL_1_1_1g.tar.gz
-tar -xzvf OpenSSL_1_1_1g.tar.gz
-cd openssl-OpenSSL_1_1_1g/
+#!/bin/bash
 
 
-sudo ./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shared zlib
-sudo make -j4
-sudo make install
+BASE="/usr/local/src"
+OPENSSL_VERSION="1.1.1g"
 
-cd /etc/ld.so.conf.d
-sudo touch openssl-1.1.1g.conf
 
-sudo echo "/usr/local/ssl/lib" >> openssl-1.1.1g.conf
+(mkdir -p ${BASE}/openssl-install \
+ && cd ${BASE}/openssl-install \
+ && curl -O -L https://github.com/openssl/openssl/archive/OpenSSL_1_1_1g.tar.gz \
+ && mkdir ${BASE}/openssl-install/openssl \
+ && tar -xvzf ${BASE}/openssl-install/OpenSSL_1_1_1g.tar.gz -C ${BASE}/openssl-install/openssl --strip-components 1 \
+ && cd ${BASE}/openssl-install/openssl \
+ && sudo ./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shared zlib \
+ && sudo make -j4 \
+ && sudo make install
+)
 
+sudo touch /etc/ld.so.conf.d/openssl-$OPENSSL_VERSION.conf
+sudo echo "/usr/local/ssl/lib" > /etc/ld.so.conf.d/openssl-$OPENSSL_VERSION.conf
 sudo ldconfig -v
-
 sudo mv /bin/openssl /bin/openssl.backup
 
-sudo touch /etc/profile.d/openssl.sh
 
+sudo touch /etc/profile.d/openssl.sh
 sudo cat >/etc/profile.d/openssl.sh <<EOL
 OPENSSL_PATH="/usr/local/ssl/bin"
 export OPENSSL_PATH
 PATH=$PATH:$OPENSSL_PATH
 export PATH
-
 EOL
 
-sudo cat >/etc/profile.d/curl.sh <<EOL
-OPENSSL_PATH="/usr/local/bin"
-export OPENSSL_PATH
-PATH=$PATH:$OPENSSL_PATH
-export PATH
-
-EOL
 sudo chmod +x /etc/profile.d/openssl.sh
-
 source /etc/profile.d/openssl.sh
-echo $PATH
 
 
-
-
-cd /usr/local/src/
-sudo wget https://www.openssl.org/source/openssl-1.1.1c.tar.gz
-
-sudo tar -xf openssl-1.1.1c.tar.gz
-
-#sudo sh -c "echo '/usr/local/openssl/lib/' >> /etc/ld.so.conf.d/openssl.conf"
 
 
