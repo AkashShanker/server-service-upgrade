3c6722167c9027351766c10c5b844bfc74387cb4
Akash Shanker <akash.shanker12@gmail.com>
2020-10-06 23:39:34 +0000

    Adding working systemd file for apache installation

--- test.sh
+++ test.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-(sudo su; VAL=${whoami}; echo $VAL)
+BASE="/home/cloud_user/server-service-upgrade"
+
+# /home/cloud_user/server-service-upgrade/system-dependecy.sh
+./system-dependency.sh
+echo "\n\n\n\n\n DONE script 1 \n\n\n\n\n\n\n"
+sleep 20
+./openssl.sh
+echo "\n\n\n\n\n DONE script 2 \n\n\n\n\n\n\n"
+sleep 20
+./curl.sh
+echo "\n\n\n\n\n DONE script 3 \n\n\n\n\n\n\n"
+sleep 5
