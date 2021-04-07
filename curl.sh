d95b95664fb379b6f7695f22baafa43b1c60d8b9
ashanker <akash.shanker12@gmail.com>
2021-04-07 00:44:29 -0500

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -7,9 +7,9 @@ CURL_VERSION="7.72.0"
 (mkdir -p ${BASE}/curl-install \
  && cd ${BASE}/curl-install \
  && curl -O -L https://curl.haxx.se/download/curl-7.72.0.tar.gz \
- && mkdir curl-${CURL_VERSION} \
- && tar -xvzf curl-7.72.0.tar.gz -C ${BASE}/curl-install/curl-${CURL_VERSION} --strip-components 1 \
- && cd ${BASE}/curl-install/curl-${CURL_VERSION} \
+ && mkdir -p ${BASE}/curl-install/curl-versions/curl-${CURL_VERSION} \
+ && tar -xvzf curl-7.72.0.tar.gz -C ${BASE}/curl-install/curl-versions/curl-${CURL_VERSION} --strip-components 1 \
+ && cd ${BASE}/curl-install/curl-versions/curl-${CURL_VERSION} \
  && sudo ./configure --prefix=/usr/local/curl --with-ssl=/usr/local/ssl \
  && sudo make -j4 \
  && sudo make install
